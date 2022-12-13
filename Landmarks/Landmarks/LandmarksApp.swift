5a832fa346129dab3e520d2b8ce8cb0caad58e9c
Aitor Trillo Díaz <aitortrillo93@gmail.com>
2022-12-13 13:11:12 +0100

    first commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Aitor Trillo Díaz on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
